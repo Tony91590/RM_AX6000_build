761cfab60d230ad1043c96665e11010cc2e0ec89
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-07-05 20:25:48 +0200

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -7,7 +7,8 @@
 #
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ntp.tencent.com/0.openwrt.pool.ntp.org/g'
-sed -i 's/ntp1.aliyun.com/1.openwrt.pool.ntp.org/g'
-sed -i 's/ntp.ntsc.ac.cn/2.openwrt.pool.ntp.org/g'
-sed -i 's/cn.ntp.org.cn/3.openwrt.pool.ntp.org/g'
+sed -i 's/ntp.tencent.com/0.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/ntp1.aliyun.com/1.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/ntp.ntsc.ac.cn/2.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/cn.ntp.org.cn/3.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/mirrors.vsean.net/openwrt/downloads.openwrt.org/g' package/emortal/default-settings/files/99-default-settings-chinese
