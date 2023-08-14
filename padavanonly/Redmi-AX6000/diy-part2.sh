bf9047521cc76122d7b8f206025510a5068c5607
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-08-14 12:34:29 +0200

    Update diy-part2.sh

--- padavanonly/Redmi-AX6000/diy-part2.sh
+++ padavanonly/Redmi-AX6000/diy-part2.sh
@@ -6,4 +6,4 @@
 # See /LICENSE for more information.
 #
 # Modify default IP
-sed -i 's/192.168.6.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.6.1/192.168.31.1/g' package/base-files/files/bin/config_generate
